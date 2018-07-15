a73e16d55e49f4fd451eeabcf100c86b9efcbb4b
Eduardo Camaratta <eduardocamaratta@gmail.com>
2018-07-15 12:08:28 -0300

    GZH paywall killer updated to work with special articles

--- GauchaZHPaywallKiller.user.js
+++ GauchaZHPaywallKiller.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         GauchaZH Paywall Killer
+// @name         GZH Paywall Killer
 // @namespace    gmcamaratta
-// @version      1.0.1
-// @description  This script disables the paywall from GauchaZH
+// @version      1.1
+// @description  This script disables the paywall from GZH
 // @author       Eduardo Camaratta
 // @match        http*://gauchazh.clicrbs.com.br/*
+// @match        http*://especiais.zh.clicrbs.com.br/*
 // @grant        none
 // @run-at       document-start
 // @downloadURL  https://github.com/eduardocamaratta/tampermonkeyscripts/raw/master/GauchaZHPaywallKiller.user.js
@@ -13,7 +14,8 @@
 // Don't execute for the root page. This is only needed because both the @match and @include tags don't support full RE syntax
 if(window.location.href.match(/^https?\:\/\/gauchazh.clicrbs.com.br\/?$/)) return;
 
-let altScriptPath = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts/35eb60ca/js/main-c349eb19faceee19b954.js";
+let altScriptPath        = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts/254d6bba/js/main-c349eb19faceee19b954.js";
+let altSpecialScriptPath = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts/254d6bba/js/special-paywall.min.js";
 
 (function() {
   window.stop();
@@ -22,6 +24,7 @@ let altScriptPath = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts
   htmlRequest.open('GET', location.href);
   htmlRequest.onload = function(event) {
     var html = htmlRequest.responseText.replace(/<script\s+src=\".*main[^\.]+.js[^\"]*\">/, '<script src="' + altScriptPath + '">');
+    html = html.replace(/http.*special-paywall.*\.js/, altSpecialScriptPath);
 
     document.open();
     document.write(html);
