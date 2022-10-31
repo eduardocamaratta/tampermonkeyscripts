50b5e6dc401d5d818d69ed1056a03880d5497e5f
Eduardo Camaratta <eduardocamaratta@gmail.com>
2022-10-31 23:25:44 +0100

    GZH Paywall Killer version 2.0. Inspired by Burlesco

--- GZHPaywallKiller.user.js
+++ GZHPaywallKiller.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GZH Paywall Killer
-// @namespace    gmcamaratta
-// @version      1.0.7
+// @namespace    gzhpaywallkiller
+// @version      2.0
 // @description  This script disables the paywall from GZH
 // @author       Eduardo Camaratta
 // @match        http*://gauchazh.clicrbs.com.br/*
@@ -14,21 +14,32 @@
 // Don't execute for the root page. This is only needed because both the @match and @include tags don't support full RE syntax
 if(window.location.href.match(/^https?\:\/\/gauchazh.clicrbs.com.br\/?$/)) return;
 
-let altScriptPath        = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts/d18aa9d6/js/main.1.65.1.min.js";
-let altSpecialScriptPath = "https://cdn.rawgit.com/eduardocamaratta/tampermonkeyscripts/407eefcc/js/special-paywall.min.js";
-
-(function() {
-  window.stop();
+document.addEventListener('DOMContentLoaded', function() {
   var htmlRequest = new XMLHttpRequest();
-
   htmlRequest.open('GET', location.href);
   htmlRequest.onload = function(event) {
-    var html = htmlRequest.responseText.replace(/<script[^m]+main[^j]+js[^\/]+\/[^\>]+\>/, '<script src="' + altScriptPath + '"></script>');
-    html = html.replace(/http.*special-paywall.*\.js/, altSpecialScriptPath);
 
-    document.open();
-    document.write(html);
-    document.close();
-  };
+      var html = htmlRequest.responseText;
+      let scriptUrl = location.origin + html.match(/\/static\/main\.[^\.]*\.min.js/)[0];
+      html = html.replace(/\/static\/main\.[^\.]*\.min.js/, "");
+
+      var scriptRequest = new XMLHttpRequest();
+      scriptRequest.open('GET', scriptUrl);
+      scriptRequest.onload = function(event) {
+          var script = scriptRequest.responseText;
+          script = script.replace(/![a-z].showLoginPaywall&&![a-z].showPaywall\|\|!1/g, 'true');
+
+          document.open();
+          document.write(html);
+          document.close();
+
+          var newScript = document.createElement('script');
+          newScript.type = 'text/javascript';
+          var textNode = document.createTextNode(script);
+          newScript.appendChild(textNode);
+          document.head.appendChild(newScript);
+      }
+      scriptRequest.send(null);
+  }
   htmlRequest.send(null);
-})();
+});
